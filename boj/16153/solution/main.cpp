2fdd142b13732bac34d1140b127a333feaa6d5b6
starplaton <starplaton@naver.com>
2024-11-19 08:36:14 +0900

    solve

--- boj/16153/solution/main.cpp
+++ boj/16153/solution/main.cpp
@@ -1,64 +1,75 @@
-#include <memory.h>
-
-#include <cstdio>
+#include <bits/stdc++.h>
 using namespace std;
+
+#define all(a) a.begin(), a.end()
+#define pii pair<ll, ll>
+#define MINI (-0x7fffffff)
+#define MAXI (0x3fffffff)
 typedef long long ll;
 
-int dp[31][10001];
-int bitMask[31];
-int maxValueSum[31];
-int minValueSum[31];
-int divisor, upperLimit;
+ll t, n, a, b;
+ll bit[31], miv[31], mav[31];
+ll dp[31][10101];
 
-int calculateSubsetCount(int bitIndex, int remainder) {
-    if (bitIndex == -1) {
-        if (remainder == 0) return 1;
+int get_dp(int x, int mo) {
+    if (x == -1) {
+        if (mo == 0) return 1;
         return 0;
     }
-    if (dp[bitIndex][remainder] != -1) return dp[bitIndex][remainder];
+    if (dp[x][mo] != -1) return dp[x][mo];
+    ll sum = get_dp(x - 1, (mo + (1 << x)) % a);
+    if (!bit[x]) sum += get_dp(x - 1, mo);
 
-    int subsetCount = calculateSubsetCount(bitIndex - 1, (remainder + (1 << bitIndex)) % divisor);
-    if (!bitMask[bitIndex]) subsetCount += calculateSubsetCount(bitIndex - 1, remainder);
-
-    return dp[bitIndex][remainder] = subsetCount;
+    return dp[x][mo] = sum;
 }
 
-int calculateValidSubsets(int bitIndex, int currentSum) {
-    if (currentSum + (ll)minValueSum[bitIndex] > upperLimit) return 0;
-    if (currentSum + (ll)maxValueSum[bitIndex] <= upperLimit) return calculateSubsetCount(bitIndex, currentSum % divisor);
+int get_count(int x, int to) {
+    if (to + miv[x] > b) return 0;
+    if (to + mav[x] <= b) return get_dp(x, to % a);
 
-    int subsetCount = calculateValidSubsets(bitIndex - 1, (currentSum + (1 << bitIndex)));
-    if (!bitMask[bitIndex]) subsetCount += calculateValidSubsets(bitIndex - 1, currentSum);
-    return subsetCount;
+    ll sum = get_count(x - 1, to + (1 << x));
+    if (!bit[x]) sum += get_count(x - 1, to);
+    return sum;
 }
 
-int main() {
-    int bitCount, bitPosition, bitMaskValue = 0, validSubsetCount = 0;
-    scanf("%d %d %d", &bitCount, &divisor, &upperLimit);
-    memset(dp, -1, sizeof(dp));
+void solve_16153() {
+    memset(dp, -1, sizeof dp);
+    cin >> n >> a >> b;
+    int bitset = 0, cnt = 0;
 
-    for (int i = 0; i < bitCount; i++) {
-        scanf("%d", &bitPosition);
-        bitMask[bitPosition] = 1;
-        bitMaskValue |= 1 << bitPosition;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        bit[x] = 1;
+        bitset |= (1 << x);
     }
 
-    if (divisor > 10000) {
-        for (ll i = divisor; i <= upperLimit; i += divisor)
-            if ((i & (ll)bitMaskValue) == (ll)bitMaskValue) validSubsetCount++;
-        return !printf("%d", validSubsetCount);
+    if (a >= 10000) {
+        for (ll i = a; i <= b; i += a) {
+            if ((i & bitset) == bitset) cnt++;
+        }
+        printf("%d\n", cnt);
+        return;
     }
 
     for (int i = 0; i < 31; i++) {
-        maxValueSum[i] = 1 << i;
-        if (i) maxValueSum[i] += maxValueSum[i - 1];
+        miv[i] = (1 << i) * bit[i];
+        if (i) miv[i] += miv[i - 1];
 
-        minValueSum[i] = (1 << i) * bitMask[i];
-        if (i) minValueSum[i] += minValueSum[i - 1];
+        mav[i] = 1 << i;
+        if (i) mav[i] += mav[i - 1];
     }
 
-    validSubsetCount = calculateValidSubsets(30, 0);
-    if (bitCount == 0) validSubsetCount--;
+    cnt = get_count(30, 0);
+    if (n == 0) cnt--;
 
-    printf("%d", validSubsetCount);
+    printf("%d\n", cnt);
+}
+
+int main() {
+    cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(false);
+    int TC = 1;
+    // scanf("%d", &TC);
+    while (TC--) solve_16153();
+    return 0;
 }
